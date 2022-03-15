10a342c779a72e60e4e798db23a234b6bb56352e
Andr3wx <apf19e@my.fsu.edu>
2022-03-15 14:19:27 -0400

    Made some small changes to last push and made some global variables

--- app.py
+++ app.py
@@ -1,16 +1,25 @@
 import pygame
 import numpy as np
 
-
 # import battleship
 # import battleshipNetwork
 # import battleshipServer
 
-# ! Make screen size global constants
+# Screen Size
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+
+# Color Values
+black = (0, 0, 0)
+white = (255, 255, 255)
+lightBlue = (173, 216, 230)
+grey = (64, 64, 64)
+
 
 # functions
 def block():
-    blockSize = (800 * 600) / 64  # Gets total area of the screen and divides it by number of squares in grid
+    blockSize = (SCREEN_WIDTH * SCREEN_HEIGHT) / 64  # Gets total area of the screen and divides it by number of
+    # squares in grid
     blockSize = blockSize ** (1 / 2)  # Square roots the total area of a single grid to get the base and width
     blockSize = blockSize / 1.5  # Shrinks the block size 1.5x of the previously calculated result
     return blockSize
@@ -20,12 +29,13 @@ def drawGrid():
     blockSize = block()
     counter2 = 0  # Makes sure there is only N number of rows
     gridLocation = {}
-    for x in np.arange(140, 800, blockSize):    # x represents row number
+    for x in np.arange(SCREEN_WIDTH*.175, SCREEN_WIDTH, blockSize):  # x represents row number
         counter1 = 0  # Makes sure there is only N number of Columns
         gridLocation[str(x)] = []
-        for y in np.arange(75, 600, blockSize): # y represents column number
+        for y in np.arange(SCREEN_HEIGHT*.1, SCREEN_HEIGHT, blockSize):  # y represents column number
             rect = pygame.Rect(x, y, blockSize, blockSize)
-            pygame.draw.rect(screen, (0, 0, 0), rect, 1)
+            pygame.draw.rect(screen, lightBlue,rect)    # Fills rectangle
+            pygame.draw.rect(screen, black, rect, 1)    # Rectangle border
             counter1 = counter1 + 1
             if len(gridLocation[str(x)]) > 0:
                 gridLocation[str(x)].append(y)
@@ -36,17 +46,17 @@ def drawGrid():
                 break
         counter2 = counter2 + 1
         if counter2 == 8:
-            gridLocation[str(x + blockSize)] = gridLocation[str(x)]     # Needed to include area of final block
+            gridLocation[str(x + blockSize)] = gridLocation[str(x)]  # Needed to include area of final block
             break
     return gridLocation
 
 
-def checkIfGrid(position, gridLocation):
+def checkIfGrid(position, locationGrid):
     gridKeys = []
     row = -1
     column = -1
     # Puts the row coordinates in a list
-    for x in gridLocation:
+    for x in locationGrid:
         gridKeys.append(float(x))
     # Sorts the rows from least to greatest
     gridKeys.sort()
@@ -54,7 +64,7 @@ def checkIfGrid(position, gridLocation):
     if gridKeys[0] <= position[0] <= gridKeys[-1]:
         for x in range(len(gridKeys)):
             if gridKeys[x] > position[0]:
-                temp = gridLocation[str(gridKeys[x - 1])]
+                temp = locationGrid[str(gridKeys[x - 1])]
                 column = x - 1
                 if temp[0] <= position[1] <= temp[-1]:
                     for y in range(len(temp)):
@@ -67,15 +77,15 @@ def checkIfGrid(position, gridLocation):
     return [row, column]
 
 
-def getRectCoord(cord, gridLocation):
+def getRectCoord(cord, locationGrid):
     tempKey = float(-1)
     tempCol = float(-1)
-    for x in gridLocation:  # Loops through keys ( Row values )
+    for x in locationGrid:  # Loops through keys ( Row values )
         if float(x) > cord[0]:  # Once finds key value greater than mouse position
-            break   # Ends loop and temp key has previous position
+            break  # Ends loop and temp key has previous position
         tempKey = float(x)
-    if tempKey != -1:   # Make sure it finds valid location
-        temp = gridLocation[str(tempKey)]
+    if tempKey != -1:  # Make sure it finds valid location
+        temp = locationGrid[str(tempKey)]
         for x in temp:  # Loops through columns in similar fashion as keys above
             if x > cord[1]:
                 break
@@ -83,15 +93,16 @@ def getRectCoord(cord, gridLocation):
     return [tempKey, tempCol]
 
 
-def mouseHighlight(position, gridLocation):
+def mouseHighlight(position, locationGrid):
     # Checks to see whether mouse is over grid
-    gridLoc = checkIfGrid(position, gridLocation)
+    gridLoc = checkIfGrid(position, locationGrid)
     if gridLoc[0] != -1 and gridLoc[1] != -1:  # If it is over grid
-        recLoc = getRectCoord(position, gridLocation)  # Get the coordinates of which rectangle the mouse is in
+        recLoc = getRectCoord(position, locationGrid)  # Get the coordinates of which rectangle the mouse is in
         if recLoc[0] != -1 and recLoc[1] != -1:  # If in a valid box
             drawGrid()
             rect = pygame.Rect(recLoc[0], recLoc[1], block(), block())
-            pygame.draw.rect(screen, (255, 255, 255), rect)  # Highlights given box
+            pygame.draw.rect(screen, white, rect)  # Highlights given box
+            pygame.draw.rect(screen, black, rect, 1)
             pygame.display.update()  # Updates display
 
     else:  # If mouse not over grid, returns grid to original state and updates display
@@ -101,7 +112,7 @@ def mouseHighlight(position, gridLocation):
 
 if __name__ == "__main__":
     pygame.init()  # initialize pygame
-    screen = pygame.display.set_mode((800, 600))  # create screen
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))  # create screen
     pygame.display.set_caption("Battleship")  # set caption
     icon = pygame.image.load('battleship.png')  # set game icon
     pygame.display.set_icon(icon)
@@ -124,7 +135,7 @@ if __name__ == "__main__":
             mouseHighlight(pos, grid)
 
         # RGB background
-        screen.fill((173, 216, 230))
+        screen.fill(black)
         if len(grid.keys()) <= 0:
             grid = drawGrid()
             pygame.display.update()
